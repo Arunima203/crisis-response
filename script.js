99aefb45df95b386ec3f062aa49835c49a3d8416
arunima <senarunima601@gmail.com>
2026-04-25 17:44:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const socket=io();
+function openPopup()   {
+    document.getElementById("popup").style.display ="block";
+}
+
+function closePopup()  {
+    document.getElementById("popup").style.display ="none";
+}
+ 
+function sendAlert() {
+    navigator.geolocation.getCurrentPosition((position => {
+        const data = {
+            lat: position.coords.latitude,
+            lng:
+  position.coords.longitude,          
+        };
+    socket.emit("sendAlert",data);
+    document.getElementById("status").innerText ="Alert Sent!";
+
+    });
+}
+
+socket.on("receiveAlert",(data) => {
+    alert("Emergency Alert!\nLocation: " + data.lat +" , " + data.lng);
+});
+
